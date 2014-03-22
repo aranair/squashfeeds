ff3dbbf33c5e74d6964d7f417e65c9e05ed43f67
Homan <boa.homan@gmail.com>
2014-03-22 09:16:36 +0800

    Move some controls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+  def get_top_back_url
+    p_controller = params[:controller] 
+    p_action = params[:action]
+    case p_controller
+    when 'matches'
+      case p_action
+      when 'show'
+        matches_path
+      when 'index'
+        ''
+      when 'edit'
+        matches_path
+      end
+    when 'team'
+    end
+  end
+
+  def get_top_content_title
+    p_controller = params[:controller] 
+    p_action = params[:action]
+    case p_controller
+    when 'matches'
+      case p_action
+      when 'show'
+        '< Back to List'
+      when 'index'
+        'UCSC'
+      when 'edit'
+        '< Back to List'
+      end
+    when 'team'
+    end
+  end
 end
