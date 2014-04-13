382bc9b6b9d5a5712e1a932f67cf399a465e2804
Homan <boa.homan@gmail.com>
2014-04-13 18:27:46 +0800

    Turbolink animations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require turbolinks_transitions
+//= require turbolinks_animations
 //= require bootstrap
 //= require_tree .
