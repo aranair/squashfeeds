e4413a82123a2af80d2a7d99970c89d3842d56f9
Homan <boa.homan@gmail.com>
2014-04-15 22:03:21 +0800

    Camel team name

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -27,15 +27,19 @@ module MatchesHelper
     "#{score}-#{4 - score}"
   end
 
+  def camel_each(str)
+    str.split.map(&:capitalize).join(' ')
+  end
+
   def render_match_title(match)
     result = match.result
     score = match.score
     if result == 'Unplayed'
       "#{match.grade.upcase}-grade. \n 
-      Against #{match.opponent.capitalize}"
+      Against #{camel_each(match.opponent)}"
     else
       "#{match.grade.upcase}-grade.  \n
-      #{format_score(match.score)} #{result} Against #{match.opponent.split.map(&:capitalize).join(' ')}"
+      #{format_score(match.score)} #{result} Against #{camel_each(match.opponent)}"
     end
   end
 end
